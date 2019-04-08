7321a8c66db961a0fee8ccaa755d87a233baa71b
JDCuevas <jd.kronoh@gmail.com>
2019-04-08 18:19:51 -0400

    Cleaning up PostDao and Handler

--- dao/PostDAO.py
+++ dao/PostDAO.py
@@ -17,6 +17,7 @@ class PostDAO:
         query = "select * from Post;"
         cursor.execute(query)
         result = []
+
         for row in cursor:
             result.append(row)
         return result
@@ -30,7 +31,7 @@ class PostDAO:
     #         result.append(row)
     #     return result
 
-    def getPostByIDForUI(self, chat_id):
+    def getChatPostsForUI(self, chat_id):
         cursor = self.conn.cursor()
         query = "SELECT post_id, post_msg, post_date, user_id, username, L.post_likes, D.post_dislikes, chat_name, location " \
                 "FROM Post natural inner join Users natural inner join Credential natural inner join Chat  natural inner join Media " \
@@ -39,8 +40,10 @@ class PostDAO:
                 "WHERE chat_id = %s;"
         cursor.execute(query, (chat_id,))
         result = []
+
         for row in cursor:
             result.append(row)
+
         return result
 
     ###################### Reaction DAO ############################
@@ -50,8 +53,10 @@ class PostDAO:
         query = "select post_id, count(*) from React where post_id = %s and react_type = 1 group by post_id;"
         cursor.execute(query, (post_id,))
         result = []
+
         for row in cursor:
             result.append(row)
+
         return result
 
     def getPostDislikesCountByID(self, post_id):
@@ -59,8 +64,10 @@ class PostDAO:
         query = "select post_id, count(*) from React where post_id = %s and react_type = -1 group by post_id;"
         cursor.execute(query, (post_id,))
         result = []
+
         for row in cursor:
             result.append(row)
+
         return result
 
     ###################### Media DAO ############################
@@ -70,8 +77,10 @@ class PostDAO:
         query = "select * from Media;"
         cursor.execute(query, (post_id,))
         result = []
+
         for row in cursor:
             result.append(row)
+
         return result
 
     ###################### Dashboard DAO ############################
@@ -81,7 +90,9 @@ class PostDAO:
         query = "select hashtag_text, count(*)  as Total from hashtag group by hashtag_text order by Total desc;"
         cursor.execute(query)
         result = []
+        
         for row in cursor:
             result.append(row)
             print(row)
+
         return result
