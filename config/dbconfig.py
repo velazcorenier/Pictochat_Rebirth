f701690f812e75fc5ec58053a51cf40903072a0d
cfboy1215 <cristian.torres2@upr.edu>
2019-05-07 20:27:52 -0400

    Change DB configuration to connect with db on Heroku.

--- config/dbconfig.py
+++ config/dbconfig.py
@@ -1,9 +1,9 @@
 #Database configuration information
 
 pg_config = {
-    'user':     'pictochat',
-    'passwd':   'pictochat',
-    'dbname':   'pictochatdb',
+    'user':     'jdlecsjnyzhxcl',
+    'passwd':   '949e1ae1ca85f0c8a9b4070a7a4bf5f69dac14ae43f3b48f372d7d3228a9a7e6',
+    'dbname':   'd9r4c5099gphrj',
     'port':     '5432',
-    'host':     'localhost'
+    'host':     'ec2-23-23-228-132.compute-1.amazonaws.com'
 }
